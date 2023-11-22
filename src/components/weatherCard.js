b7b4e2af1fd53f8aebc4306beb4ff50e8b123e27
darrionn33 <darrionn33@gmail.com>
2023-11-22 09:01:51 +0530

    added AM and PM to weather card

--- src/components/weatherCard.js
+++ src/components/weatherCard.js
@@ -8,18 +8,30 @@ const weatherCard = (weatherData) => {
   );
 
   const conditionIcon = put("img.condition-img", {
-    src: weatherData.current.condition.icon,
+    src: weatherData.current.condition.icon.split("64x64").join("128x128"),
   });
 
   const infoDiv = put("div.info");
   const temperature = put("h3.temp", weatherData.current["temp_c"] + "°C");
   const condition = put("h3.condition", weatherData.current.condition.text);
-
+  // to set time and check if AM or PM
+  let n = weatherData.location.localtime.split(" ")[1].split("");
+  if (n.length < 5) {
+    n.unshift("0");
+  }
+  const time = n.join("");
+  let ext = "AM";
+  if (+(n[0] + n[1]) >= 12) {
+    ext = "PM";
+  }
+  // to set time and check if AM or PM
+  const timeH3 = put("h3.time", time + ext);
   put(weatherCard, title);
   put(weatherCard, conditionIcon);
   put(weatherCard, infoDiv);
   put(infoDiv, temperature);
   put(infoDiv, condition);
+  put(infoDiv, timeH3);
   return weatherCard;
 };
 
